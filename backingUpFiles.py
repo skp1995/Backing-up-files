fb0f5f7be54706c67b77474332ef81c50393de09
skp1995 <shreeekrish@gmail.com>
2017-08-25 12:11:50 -0700

    Add files via upload

---
+++ backingUpFiles.py
@@ -0,0 +1,36 @@
+import zipfile, os
+
+def backup(folder):
+    folder = os.path.abspath(folder)
+    n = 1
+    while True:
+        zip1 = os.path.basename(folder) + '_' + str(n) + '.zip'
+        if not os.path.exists(zip1):
+                break
+        n = n + 1
+
+    print('Creating %s...' % (zip1))
+ 
+    backupZip = zipfile.ZipFile(zip1, 'w') 
+
+    for f, subf, filen in os.walk(folder):
+        print('Adding files in %s...' % (f))
+           # Add the current folder to the ZIP file.
+        backupZip.write(f)
+           # Add all the files in this folder to the ZIP file.
+        for filename in filen:
+            if not filename.endswith(''):
+                print(filename)
+                newBase = os.path.basename(folder) + '_'
+                # Remember:  Do not back up the zip files
+                if filename.startswith(newBase) and filename.endswith('.zip'):
+                    continue   
+                backupZip.write(os.path.join(f, filename))
+    backupZip.close()    
+
+    print('Done.')
+
+backup('C:\\Test')
+
+
+
